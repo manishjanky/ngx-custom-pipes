1c5c1c86637d534d61e60d674b45c1c2d66e740d
manishjanky <manishjanky@gmail.com>
2018-03-28 15:24:11 +0530

    string pipes module [skip ci]

--- src/string-pipes/string-pipes.module.ts
+++ src/string-pipes/string-pipes.module.ts
@@ -1,9 +1,29 @@
+import { StringUcwordsFirstPipe } from './pipes/string-ucwords-first.pipe';
+import { StringUcfirstPipe } from './pipes/string-ucfirst.pipe';
+import { StringTrimPipe } from './pipes/string-trim.pipe';
+import { StringRtrimPipe } from './pipes/string-rtrim.pipe';
+import { StringReversePipe } from './pipes/string-reverse.pipe';
+import { StringReplaceLastPipe } from './pipes/string-replace-last.pipe';
+import { StringReplaceFirstPipe } from './pipes/string-replace-first.pipe';
+import { StringRepeatPipe } from './pipes/string-repeat.pipe';
+import { StringProfileName } from './pipes/string-profile-name.pipe';
+import { StringCamelCasePipe } from './pipes/string-camel-case.pipe';
+import { StringAbbrevCurrencyPipe } from './pipes/string-abbrev-currency.pipe';
+import { StringLtrimPipe } from './pipes/string-ltrim.pipe';
+import { StringPadEndPipe } from './pipes/string-pad-end.pipe';
+import { StringPadStartPipe } from './pipes/string-pad-start.pipe';
+import { StringReplacePipe } from './pipes/string-replace.pipe';
 import { NgModule } from "@angular/core";
 
 @NgModule({
-  declarations: [ ],
+  declarations: [StringAbbrevCurrencyPipe, StringCamelCasePipe, StringLtrimPipe, StringPadEndPipe, StringPadStartPipe,
+    StringProfileName, StringRepeatPipe, StringReplaceFirstPipe, StringReplaceLastPipe, StringReplacePipe,
+    StringReversePipe, StringRtrimPipe, StringTrimPipe, StringUcfirstPipe, StringUcwordsFirstPipe],
   imports: [],
-  exports: [ ],
+  exports: [StringAbbrevCurrencyPipe, StringCamelCasePipe, StringLtrimPipe, StringPadEndPipe, StringPadStartPipe,
+    StringProfileName, StringRepeatPipe, StringReplaceFirstPipe, StringReplaceLastPipe, StringReplacePipe,
+    StringReversePipe, StringRtrimPipe, StringTrimPipe, StringUcfirstPipe, StringUcwordsFirstPipe],
   providers: [],
-  bootstrap: []})
-export class StringPipesModule {}
+  bootstrap: []
+})
+export class StringPipesModule { }
